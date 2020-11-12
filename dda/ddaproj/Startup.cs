5bb5b0e607b382e7e330249bd5d888227ad56f1d
gibsgibs-desktop <g.brehm@hotmail.com>
2020-11-11 21:56:20 -0800

    Attempting to remove the need to alter the connection string before deployment.

--- dda/ddaproj/Startup.cs
+++ dda/ddaproj/Startup.cs
@@ -1,12 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using ddaproj.Data;
 using Microsoft.Extensions.Configuration;
@@ -29,7 +23,14 @@ namespace ddaproj
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DDADB_Azure")));
+            try
+            {
+                services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["DDADB_Azure"]));
+            }
+            catch
+            {
+                services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DDADB_Azure")));
+            }
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
