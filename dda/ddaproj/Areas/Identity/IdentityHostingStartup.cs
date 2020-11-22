588b426129a1f77b7b785c4a0508f5f8a4964870
gibsgibs-desktop <g.brehm@hotmail.com>
2020-11-21 22:35:53 -0800

    Updated the register page.

---
+++ dda/ddaproj/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ddaproj.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ddaproj.Areas.Identity.IdentityHostingStartup))]
+namespace ddaproj.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
