335c62b1ada37fce06c2dd4aba4cac02cb2a444f
gibsgibs-desktop <g.brehm@hotmail.com>
2020-11-25 15:21:53 -0800

    Superadmin has been seeded.

--- dda/ddaproj/Startup.cs
+++ dda/ddaproj/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ddaproj.Data.Models;
+using ddaproj.Utilities;
 
 namespace ddaproj
 {
@@ -33,7 +34,7 @@ namespace ddaproj
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, UserManager<ApplicationUser> userManager)
         {
             if (env.IsDevelopment())
             {
@@ -54,6 +55,8 @@ namespace ddaproj
 
             app.UseAuthentication();
             app.UseAuthorization();
+
+            IdentityDataInitializer.SeedSuperAdmin(userManager);
 
             app.UseEndpoints(endpoints =>
             {
---
+++ dda/ddaproj/Utilities/IdentityDataInitializer.cs
@@ -0,0 +1,29 @@
+﻿using ddaproj.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace ddaproj.Utilities
+{
+    public class IdentityDataInitializer
+    {
+        public static void SeedSuperAdmin(UserManager<ApplicationUser> userManager)
+        {
+            var SuperAdminUsername = Startup.StaticConfiguration["SuperAdmin:Username"];
+            if (userManager.FindByNameAsync(SuperAdminUsername).Result != null)
+            {
+                return;
+            }
+            var user = new ApplicationUser
+            {
+                UserName = SuperAdminUsername,
+                Email = Startup.StaticConfiguration["SuperAdmin:Email"],
+                EmailConfirmed = true
+            };
+            var result = userManager.CreateAsync(user, Startup.StaticConfiguration["SuperAdmin:Password"]).Result;
+            if (result.Succeeded)
+            {
+                userManager.AddClaimAsync(user, new Claim(ClaimTypes.Role, "SuperAdmin")).Wait();
+            }
+        }
+    }
+}
