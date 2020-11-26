34ef38b7c954b32823eac8603b953e86025c4e0e
gibsgibs-desktop <g.brehm@hotmail.com>
2020-11-25 16:49:57 -0800

    Added a claim table.

---
+++ dda/ddaproj/Data/Models/UserClaim.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ddaproj.Data.Models
+{
+    public class UserClaim
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
